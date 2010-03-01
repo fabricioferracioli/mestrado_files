54af519219dd644c60a22326e7cd0232cf15fc69
Fabricio Ferracioli <fabricioferracioli@gmail.com>
2010-03-01 11:14:13 -0300

    vai ser necessario verificar a melhor maneira de trabalhar com os acessos, para tratar do problema de sessoes abertas de usuarios simultaneamente

--- logutil.py
+++ logutil.py
@@ -2,6 +2,8 @@
 #!/usr/bin/pyhton
 #funcoes uteis na manipulacao dos arquivos de log
 
+import datetime
+
 class LogUtil:
 
     def getRequester(self, logline):
@@ -18,3 +20,33 @@ class LogUtil:
 
     def getResponseSize(self, logline):
         return logline[logline.rindex('"'):].split()[2]
+
+    def calculateDiferenceBetweenTimes(self, beginTime, endTime)
+        return datetime.datetime(beginTime['year'], beginTime['month'], beginTime['day'], beginTime['hour'], beginTime['minute'], beginTime['second']) - datetime.datetime(endTime['year'], endTime['month'], endTime['day'], endTime['hour'], endTime['minute'], endTime['second'])
+
+    def createNNInputFile(self, access, filename, maxurls, maxIdleSessionTime = 20):
+        nnfile = open(filename, 'w')
+        session_time = []
+        actual_user = ''
+        line = ''
+        for ac in access:
+            if actual_user == ac['user_id']:
+                #primeiro preciso finalizar a linha atual entao termina isso aqui depois
+                #quando for o mesmo usuario da ultima url, posso estar dentro de uma sessao
+                time_between_requests = calculateDiferenceBetweenTimes(session_time.pop(), ac['request_date'])
+                if  time_between_requests <= maxIdleSessionTime:
+                    #estou na mesma sessao
+                    line += time_between_requests +' 'ac['page_id']+' '
+                    session_time.append(ac['request_date'])
+                else:
+                    #nao estou na mesma sessao, entao finalizo a linha com o tempo maximo permitido e gravo no arquivo
+                    line += maxIdleSessionTime
+                    nnfile.write(line)
+                    #entao inicio uma nova linha com a nova sessao do mesmo usuario
+                    line = ac['page_id']
+                    session_time.pop()
+                    session_time.append(ac['request_date'])
+            else
+                #se for um novo usuario apenas inicio uma nova linha com a id da pagina acessada
+                line += ac['page_id']+' '
+                session_time.append(ac['request_date'])
