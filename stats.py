708283f91f37fb22d008a890c99343b28f92b556
Fabrício Ferracioli <fabricioferracioli@gmail.com>
2011-06-04 19:28:08 -0300

    rotina que calcula a variancia da amostra, obtendo o f-value e o wilk lambda

---
+++ stats.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/pyhton
+#http://academic.emporia.edu/mooredwi/rda/notes10.htm
+#http://academic.emporia.edu/mooredwi/rda/notes4.htm
+#http://www.westgard.com/lesson40.htm
+
+class Stats:
+
+    def total_variance(self, distribution):
+        sumX = []
+        Xbar = []
+        Xbar2 = []
+        sumX2 = []
+        sum_squares = []
+        within_groups_dg_freedom = 0
+
+        for X in distribution:
+            sumX.append(sum(X))
+            Xbar.append(float(sumX[len(sumX) - 1]) / len(X))
+            Xbar2.append(Xbar[len(Xbar) - 1] ** 2)
+            sumX2.append(sum([a**2 for a in X]))
+            sum_squares.append(sum([(a - Xbar[len(Xbar) - 1])**2 for a in X]))
+            within_groups_dg_freedom += len(X) - 1
+
+        within_groups_sum_squares = sum(sum_squares)
+        within_groups_variance = within_groups_sum_squares / within_groups_dg_freedom
+        grand_mean = sum(Xbar) / len(Xbar)
+        variance_means = sum([(a - grand_mean)**2 for a in Xbar])/(len(distribution) - 1)
+        among_groups_variance = len(distribution[0]) * variance_means
+        f_value = among_groups_variance / within_groups_variance
+
+        total_variance = 0
+        for X in distribution:
+            for x in X:
+                total_variance = (x - grand_mean)**2
+
+        wilk_lambda = within_groups_variance / total_variance
+
+        print 'Within groups variance: %f'%within_groups_variance
+        print 'Among groups variance: %f'%among_groups_variance
+        print 'Total variance: %f'%total_variance
+        print 'F-value: %f'%f_value
+        print 'Wilk\'s Lambda: %f'%wilk_lambda
+
+st = Stats()
+st.total_variance([[41,44,48,43,42], [48,49,49,49,45], [40,50,44,48,50], [40,39,46,46,41], [49,41,50,39,42], [40,48,51,47,51], [41,46,54,44,42]])
